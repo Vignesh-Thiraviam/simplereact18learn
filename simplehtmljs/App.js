76184144d9240b527860d1b2b03ed6d4815992c0
Vignesh-Thiraviam <thiraviammvignesh@gmail.com>
2024-07-07 14:59:09 +0100

    Add files via upload
    Episode01

---
+++ simplehtmljs/App.js
@@ -0,0 +1,9 @@
+const parent = React.createElement("div" , { id : "parent"}, React.createElement("div" , { id : "child"},
+[React.createElement("h1" , {}, "Hello world form react child"),React.createElement("h2" , {}, "Hello world form react child 2")]));
+
+
+const heading = React.createElement("h1" , { id : "heading"}, "Hello world form react");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
